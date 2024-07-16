bca191b2484b2fb74c03b6f4bfda65370cfcae39
Craig Duggan <craigduggan90@icloud.com>
2024-07-16 10:08:46 +0100

    refactor: remove restrictive enums

--- src/Hooks.Service/UseCases/PublishWebhookEvent/PublishWebhookEventCommandValidator.cs
+++ src/Hooks.Service/UseCases/PublishWebhookEvent/PublishWebhookEventCommandValidator.cs
@@ -1,24 +1,18 @@
 using System.Text.RegularExpressions;
 using FluentValidation;
-using Hooks.Common.Enums;
 
 namespace Hooks.Service.UseCases.PublishWebhookEvent;
 
 public class PublishWebhookEventCommandValidator : AbstractValidator<PublishWebhookEventCommand>
 {
     internal const string IncorrectFormat = "Incorrect format";
-    internal const string UnsupportedTopic = "Unsupported topic";
     public PublishWebhookEventCommandValidator()
     {
         // Topic must be in correct format and each component must be supported
         var topicRegex = new Regex(@"^[a-z]+\.[a-z]+$");
         RuleFor(command => command.Topic)
-            .Must(topic =>
-                topicRegex.IsMatch(topic)
-                && EntityType.TryGetValue(topic.Split('.').ElementAtOrDefault(0), out _)
-                && EventType.TryGetValue(topic.Split('.').ElementAtOrDefault(1), out _)
-            )
-            .WithMessage(UnsupportedTopic);
+            .Must(topic => topicRegex.IsMatch(topic))
+            .WithMessage(IncorrectFormat);
         
         // Uri must be valid
         RuleFor(command => command.Uri)
